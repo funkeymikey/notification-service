a98602f3e14262187a51a17c437120b752b5943e
Mike Kozelsky <mike.kozelsky@gmail.com>
2015-03-04 10:02:41 -0500

    removed heartbeat, going to check pricing and usage in azure for a while

--- Notification.Core/NotificationControlCenter.cs
+++ Notification.Core/NotificationControlCenter.cs
@@ -14,7 +14,7 @@ namespace Notification.Core
         private String _appName;
         private HubConnection _hubConnection;
         private IHubProxy _proxy;
-        private Timer _heartbeatTimer;        
+				//private Timer _heartbeatTimer;        
         
         public NotificationControlCenter(String appName, String appID)
         {
@@ -23,8 +23,8 @@ namespace Notification.Core
             _appName = appName;
 
             //every thirty seconds, try and connect with the hub
-            _heartbeatTimer = new Timer(30 * 1000);
-            _heartbeatTimer.Elapsed += CheckHeartbeat;
+						//_heartbeatTimer = new Timer(30 * 1000);
+						//_heartbeatTimer.Elapsed += CheckHeartbeat;
 
             //put the shortcut on start menu if not there already
             ShortcutHelper.InitializeShortcut(_appName, _appID);
@@ -48,8 +48,8 @@ namespace Notification.Core
                 await _hubConnection.Start();
 
                 //start the timer if it's not started
-                if (!_heartbeatTimer.Enabled)
-                    _heartbeatTimer.Start();
+								//if (!_heartbeatTimer.Enabled)
+								//		_heartbeatTimer.Start();
 
                 Logger.Log("Connection started");
             }
@@ -59,31 +59,31 @@ namespace Notification.Core
         public void StopConnection()
         {
             _hubConnection.Dispose();
-            _heartbeatTimer.Dispose();
+						//_heartbeatTimer.Dispose();
 
             Logger.Log("Connection closed");
         }
 
-        private async void CheckHeartbeat(object sender, ElapsedEventArgs e)
-        {
-            //don't do anything if we're attempting to connect
-            if (_hubConnection.State == ConnectionState.Connecting || _hubConnection.State == ConnectionState.Reconnecting)
-                return;
+				//private async void CheckHeartbeat(object sender, ElapsedEventArgs e)
+				//{
+				//		//don't do anything if we're attempting to connect
+				//		if (_hubConnection.State == ConnectionState.Connecting || _hubConnection.State == ConnectionState.Reconnecting)
+				//				return;
 
-            try
-            {
-                //try to contact the server
-                await _proxy.Invoke("Heartbeat");
-            }
-            catch (Exception)
-            {
-                //failure, so toss the current connection in the garbage
-                _hubConnection.Dispose();
+				//		try
+				//		{
+				//				//try to contact the server
+				//				await _proxy.Invoke("Heartbeat");
+				//		}
+				//		catch (Exception)
+				//		{
+				//				//failure, so toss the current connection in the garbage
+				//				_hubConnection.Dispose();
 
-                //try to reconnect
-                this.StartConnection();
-            }
-        }
+				//				//try to reconnect
+				//				this.StartConnection();
+				//		}
+				//}
 
         //invoked when the hub says "notify"
         private void AddNotification(NotificationMessage newNotification)
